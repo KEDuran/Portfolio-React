8c1aa3c1b1930c1495524d07bbc2714f9ae12439
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-08 16:22:12 -0500

    removed unneded import

--- src/pages/FunFacts/index.js
+++ src/pages/FunFacts/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./style.css";
-import ContactPage from "../Contact";
 
 function FunFactsPage() {
     return (
