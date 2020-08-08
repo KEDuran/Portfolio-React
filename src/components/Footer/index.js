d6c405c7f226fa632ef570acdf5250443aa2c3a0
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-07 22:47:38 -0500

    modified footer responsiveness

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,20 +5,25 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
 	return (
-		<Navbar className="purple" fixed="bottom">
+		<Navbar
+			className="purple d-flex flex-column flex-sm-row align-items-center align-horizontal"
+			fixed="bottom"
+		>
 			<Nav.Item>
-				<p className="text-light">&copy; 2020 Krystal E. Duran</p>
+				<a href="#" className="text-light">
+					&copy; 2020 Krystal E. Duran
+				</a>
 			</Nav.Item>
 			<Nav.Item>
 				<a
 					href="https://www.linkedin.com/in/krystal-e-duran-b64552123/"
-					className="text-light mr-3"
+					className="text-light "
 				>
 					<FaLinkedin /> LinkedIn
 				</a>
 			</Nav.Item>
 			<Nav.Item>
-				<a href="https://github.com/KEDuran" class="text-light mr-3">
+				<a href="https://github.com/KEDuran" class="text-light">
 					<FaGithub /> GitHub
 				</a>
 			</Nav.Item>
