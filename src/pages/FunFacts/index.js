63d662c0e35494f682a74b09b89841f4ec4e12e8
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-08 18:22:24 -0500

    added container grid into FunFacts page

--- src/pages/FunFacts/index.js
+++ src/pages/FunFacts/index.js
@@ -3,13 +3,37 @@ import { Container, Row, Col } from "react-bootstrap";
 import "./style.css";
 
 function FunFactsPage() {
-    return (
-        <Container>
-            <Row></Row>
-            <Row></Row>
-            <Row></Row>
-        </Container>
-    )
+	return (
+		<Container>
+			<Row>
+				<h3 className="mt-5 mb-3">Fun Facts</h3>
+			</Row>
+			<Row>
+				<Col sm={1} md={1} lg={2}></Col>
+				<Col sm={10} md={10} lg={8}>
+					<FunFactsCard />
+					<FunFactsCard />
+				</Col>
+				<Col sm={1} md={1} lg={2}></Col>
+			</Row>
+			<Row>
+				<Col sm={1} md={1} lg={2}></Col>
+				<Col sm={10} md={10} lg={8}>
+					<FunFactsCard />
+					<FunFactsCard />
+				</Col>
+				<Col sm={1} md={1} lg={2}></Col>
+			</Row>
+			<Row>
+				<Col sm={1} md={1} lg={2}></Col>
+				<Col sm={10} md={10} lg={8}>
+					<FunFactsCard />
+					<FunFactsCard />
+				</Col>
+				<Col sm={1} md={1} lg={2}></Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default FunFactsPage;
