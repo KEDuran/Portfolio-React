1b2bddf3906f27bdc089ecab7f30a3aa8a21aab6
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-07 22:17:48 -0500

    added react icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import "./style.css";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
 	return (
@@ -13,12 +14,12 @@ function Footer() {
 					href="https://www.linkedin.com/in/krystal-e-duran-b64552123/"
 					className="text-light mr-3"
 				>
-					<i className="fab fa-linkedin"></i> LinkedIn
+					<FaLinkedin /> LinkedIn
 				</a>
 			</Nav.Item>
 			<Nav.Item>
 				<a href="https://github.com/KEDuran" class="text-light mr-3">
-					<i className="fab fa-github"></i> GitHub
+					<FaGithub /> GitHub
 				</a>
 			</Nav.Item>
 		</Navbar>
