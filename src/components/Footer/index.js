a4f854c69247c48c5abbb3866357186ada921638
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-07 22:54:21 -0500

    fixed margin around footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,20 +10,20 @@ function Footer() {
 			fixed="bottom"
 		>
 			<Nav.Item>
-				<a href="#" className="text-light">
+				<a href="#" className="text-light m-3">
 					&copy; 2020 Krystal E. Duran
 				</a>
 			</Nav.Item>
 			<Nav.Item>
 				<a
 					href="https://www.linkedin.com/in/krystal-e-duran-b64552123/"
-					className="text-light "
+					className="text-light m-3"
 				>
 					<FaLinkedin /> LinkedIn
 				</a>
 			</Nav.Item>
 			<Nav.Item>
-				<a href="https://github.com/KEDuran" class="text-light">
+				<a href="https://github.com/KEDuran" className="text-light m-3">
 					<FaGithub /> GitHub
 				</a>
 			</Nav.Item>
