785270431eb2d840dba35316ada427747fd0770c
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-08 00:58:53 -0500

    imported required components/files for contact page

---
+++ src/pages/contact/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+import BtnComponent from "../../components/Button";
+import InputTextForm from "../../components/InputTextForm";
+import InputTextArea from "../../components/InputTextArea";
+
+function ContactPage(props) {
+    return()
+}
+export default ContactPage;
