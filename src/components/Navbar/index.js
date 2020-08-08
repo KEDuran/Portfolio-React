9b973700c0d3cfc60d81c25305f72c6d218736f6
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-07 21:28:16 -0500

    created initial nav bar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "./style.css";
+
+function NavbarComp() {
+	return (
+		<Navbar className="purple" expand="lg">
+			<Navbar.Brand href="#home">
+				<h4 className="text-light">Krystal E. Duran</h4>
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+				<Nav className="text-light">
+					<Nav.Link className="text-light" href="#home">
+						About Me
+					</Nav.Link>
+					<Nav.Link className="text-light" href="#link">
+						Projects
+					</Nav.Link>
+					<Nav.Link className="text-light" href="#link">
+						Fun Facts
+					</Nav.Link>
+					<Nav.Link className="text-light" href="#link">
+						Contact
+					</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
+}
+
+export default NavbarComp;
