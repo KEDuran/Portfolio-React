868b4701fe878ad6f2ea3a410b93cbad2cd7d908
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-08 21:53:20 -0500

    created basic structure for projects page

---
+++ src/pages/Projects/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import "./style.css";
+
+function ProjectPage() {
+	return (
+		<Container>
+			<Row></Row>
+			<Row></Row>
+			<Row></Row>
+			<Row></Row>
+		</Container>
+	);
+}
+
+export default ProjectPage;
