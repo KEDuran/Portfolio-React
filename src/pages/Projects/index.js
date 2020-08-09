8beb7e9842acb446b2bba751902fc3174c662e8d
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-08 22:45:54 -0500

    added images for project page

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -6,7 +6,7 @@ import ProjectCard from "../../components/ProjectCard";
 function ProjectPage() {
 	return (
 		<Container>
-			<Row>
+			<Row className="mt-5">
 				<Col sm={1} md={1} lg={1}></Col>
 				<Col sm={10} md={10} lg={10}>
 					<h3 className="mb-3">Projects</h3>
@@ -14,8 +14,8 @@ function ProjectPage() {
 				<Col sm={1} md={1} lg={1}></Col>
 			</Row>
 			<Row>
-				<Col sm={1} md={1} lg={1}></Col>
-				<Col sm={5} md={5} lg={5}>
+				<Col sm={1} md={1} lg={1} className="mb-4"></Col>
+				<Col sm={5} md={5} lg={5} className="mb-4">
 					<ProjectCard />
 				</Col>
 				<Col sm={5} md={5} lg={5}>
@@ -24,8 +24,8 @@ function ProjectPage() {
 				<Col sm={1} md={1} lg={1}></Col>
 			</Row>
 			<Row>
-				<Col sm={1} md={1} lg={1}></Col>
-				<Col sm={5} md={5} lg={5}>
+				<Col sm={1} md={1} lg={1} className="mb-4"></Col>
+				<Col sm={5} md={5} lg={5} className="mb-4">
 					<ProjectCard />
 				</Col>
 				<Col sm={5} md={5} lg={5}>
@@ -34,8 +34,8 @@ function ProjectPage() {
 				<Col sm={1} md={1} lg={1}></Col>
 			</Row>
 			<Row>
-				<Col sm={1} md={1} lg={1}></Col>
-				<Col sm={5} md={5} lg={5}>
+				<Col sm={1} md={1} lg={1} className="mb-4"></Col>
+				<Col sm={5} md={5} lg={5} className="mb-4">
 					<ProjectCard />
 				</Col>
 				<Col sm={5} md={5} lg={5}>
