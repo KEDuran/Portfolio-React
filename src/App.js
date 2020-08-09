44cddb9dc9cbe311bf09e57e71ecd24ddcc14da1
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-08 23:50:21 -0500

    added bio page into navbar route

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import NavbarComp from "./components/Navbar";
 import ContactPage from "./pages/Contact";
 import FunFactsPage from "./pages/FunFacts";
 import ProjectPage from "./pages/Projects";
+import BioPage from "./pages/Bio";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import BioPage from "./pages/Bio";
 
 function App() {
 	return (
 		<Router>
 			<NavbarComp />
 			<Switch>
+				<Route path="/" exact component={BioPage} />
 				<Route path="/contact" component={ContactPage} />
 				<Route path="/funfacts" component={FunFactsPage} />
 				<Route path="/projects" component={ProjectPage} />
---
+++ src/pages/Bio/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Container, Col, Row, Card } from "react-bootstrap";
+import "./style.css";
+
+function BioPage() {}
+
+export default BioPage;
