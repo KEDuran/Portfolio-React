0f639abd3cad0cff46d30cc7a1278622b9036bc1
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-26 18:49:49 -0500

    modified the contact component and package.json start command

--- src/components/InputTextForm/index.js
+++ src/components/InputTextForm/index.js
@@ -1,20 +1,29 @@
 import React from "react";
-import { InputGroup, FormControl } from "react-bootstrap";
+import { InputGroup, FormControl, Form } from "react-bootstrap";
 import "./style.css";
 
 function InputTextForm(props) {
 	return (
-		<InputGroup className="mb-3">
-			<InputGroup.Prepend>
-				<InputGroup.Text id="inputGroup-sizing-default" className=" text-light green">
-					{props.name}
-				</InputGroup.Text>
-			</InputGroup.Prepend>
-			<FormControl
-				aria-label="Default"
-				aria-describedby="inputGroup-sizing-default"
-			/>
-		</InputGroup>
+		<Form>
+			<Form.Group controlId="exampleForm.ControlInput1">
+				<Form.Label>{props.name}</Form.Label>
+				<Form.Control type={props.type} placeholder={props.placeholder} />
+			</Form.Group>
+		</Form>
+		// <InputGroup className="mb-3">
+		// 	<InputGroup.Prepend>
+		// 		<InputGroup.Text
+		// 			id="inputGroup-sizing-default"
+		// 			className=" text-light green"
+		// 		>
+		// 			{props.name}
+		// 		</InputGroup.Text>
+		// 	</InputGroup.Prepend>
+		// 	<FormControl
+		// 		aria-label="Default"
+		// 		aria-describedby="inputGroup-sizing-default"
+		// 	/>
+		// </InputGroup>
 	);
 }
 
