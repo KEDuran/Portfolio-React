29c1213958f17a03fffbe8dc5f5a2c001122fc26
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-26 21:34:47 -0500

    refactored the fun facts cards

--- src/assets/funFactsData.js
+++ src/assets/funFactsData.js
@@ -1,20 +1,22 @@
 import SanBenito from "../pages/FunFacts/images/SanBenito.png";
 import UTPA from "../pages/FunFacts/images/UTPA.gif";
 import Pflugerville from "../pages/FunFacts/images/Pflugerville.png";
-import Disney from "../pages/FunFacts/images/Pflugerville.png";
+import Disney from "../pages/FunFacts/images/Disney.jpg";
 import Caleb from "../pages/FunFacts/images/Caleb.jpg";
 import TASB from "../pages/FunFacts/images/TASB.jpg";
 
 export default [
 	{
 		id: 1,
+		header: "Fun Fact # 1",
 		title: "Born and Raised",
 		image: SanBenito,
 		message:
 			"I grew up in San Benito, Texas located in the Rio Grande Valley. I come from a very close-knit Hispanic family!",
 	},
 	{
-		id: 1,
+		id: 2,
+		header: "Fun Fact # 2",
 		title: "UTPA Alumni",
 		image: UTPA,
 		message:
@@ -22,6 +24,7 @@ export default [
 	},
 	{
 		id: 3,
+		header: "Fun Fact # 3",
 		title: "My Home",
 		image: Pflugerville,
 		message:
@@ -29,6 +32,7 @@ export default [
 	},
 	{
 		id: 4,
+		header: "Fun Fact # 4",
 		title: "The Dynamic Duo",
 		image: Disney,
 		message:
@@ -36,6 +40,7 @@ export default [
 	},
 	{
 		id: 5,
+		header: "Fun Fact # 5",
 		title: "Our Pupper, Caleb",
 		image: Caleb,
 		message:
@@ -44,6 +49,7 @@ export default [
 	{
 		id: 6,
 		title: "Halloween at TASB",
+		header: "Fun Fact # 6",
 		image: TASB,
 		message:
 			"I have worked for the Texas Association of School Boards (TASB) since 2018. I work with a great team of fun and smart people!",
--- src/pages/FunFacts/index.js
+++ src/pages/FunFacts/index.js
@@ -1,70 +1,34 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import FunFactsCard from "../../components/FunFactsCard";
+import funFactsData from "../../assets/funFactsData";
 
 function FunFactsPage() {
+	const funFactsList = funFactsData.map((data) => (
+		<FunFactsCard
+			key={data.id}
+			header={data.header}
+			image={data.image}
+			title={data.title}
+			message={data.message}
+		/>
+	));
+
 	return (
 		<Container>
 			<Row className="mt-5">
-				<Col sm={1} md={1} lg={1}></Col>
-				<Col sm={10} md={10} lg={10}>
+				<Col sm={2} md={2} lg={2}></Col>
+				<Col sm={8} md={8} lg={8}>
 					<h3 className="mb-3">Fun Facts</h3>
 				</Col>
-				<Col sm={1} md={1} lg={1}></Col>
+				<Col sm={2} md={2} lg={2}></Col>
 			</Row>
-			<Row>
-				<Col sm={1} md={1} lg={1} className="mb-4"></Col>
-				<Col sm={5} md={5} lg={5} className="mb-4">
-					<FunFactsCard
-						src={require("./images/SanBenito.png")}
-						title="Born and Raised"
-						message="I grew up in San Benito, Texas located in the Rio Grande Valley. I come from a very close-knit Hispanic family!"
-					/>
+			<Row className="mt-5">
+				<Col sm={2} md={2} lg={2}></Col>
+				<Col sm={8} md={8} lg={8}>
+					{funFactsList}
 				</Col>
-				<Col sm={5} md={5} lg={5}>
-					<FunFactsCard
-						src={require("./images/UTPA.gif")}
-						title="UTPA Alumni"
-						message="I attended the University of Texas Pan American (now renamed to the University of Texas Rio Grande Valley)."
-					/>
-				</Col>
-				<Col sm={1} md={1} lg={1}></Col>
-			</Row>
-			<Row>
-				<Col sm={1} md={1} lg={1} className="mb-4"></Col>
-				<Col sm={5} md={5} lg={5} className="mb-4">
-					<FunFactsCard
-						src={require("./images/Pflugerville.png")}
-						title="My Home"
-						message="My husband, James, and I have lived in Pflugerville, Texas (aka a city between a rock and a weird place) since 2016!"
-					/>
-				</Col>
-				<Col sm={5} md={5} lg={5}>
-					<FunFactsCard
-						src={require("./images/Disney.jpg")}
-						title="The Dynamic Duo"
-						message="James and I are a small but mighty family! We work together to provide the best life for us and our pupper."
-					/>
-				</Col>
-				<Col sm={1} md={1} lg={1}></Col>
-			</Row>
-			<Row className="mb-5">
-				<Col sm={1} md={1} lg={1} className="mb-4"></Col>
-				<Col sm={5} md={5} lg={5} className="mb-4">
-					<FunFactsCard
-						src={require("./images/Caleb.jpg")}
-						title="Our Pupper, Caleb"
-						message="Caleb has been a wonderful part of our lives for the past 11 years. He is a super cute pupper that likes rocking some snazzy shoes!"
-					/>
-				</Col>
-				<Col sm={5} md={5} lg={5}>
-					<FunFactsCard
-						src={require("./images/TASB.jpg")}
-						title="Halloween at TASB"
-                        message="I have worked for the Texas Association of School Boards (TASB) since 2018. I work with a great team of fun and smart people!"
-					/>
-				</Col>
-				<Col sm={1} md={1} lg={1}></Col>
+				<Col sm={2} md={2} lg={2}></Col>
 			</Row>
 		</Container>
 	);
