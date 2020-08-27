6f50636f5c527143fea61ab8f4905bc2f4b966a0
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-26 19:05:48 -0500

    removed pseudocode and unnecessary imports

--- src/components/InputTextArea/index.js
+++ src/components/InputTextArea/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { InputGroup, FormControl, Form } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import "./style.css";
-import { First } from "react-bootstrap/esm/PageItem";
 
 function InputTextArea(props) {
 	return (
@@ -11,12 +10,6 @@ function InputTextArea(props) {
 				<Form.Control as="textarea" rows="3" />
 			</Form.Group>
 		</Form>
-		// <InputGroup>
-		// 	<InputGroup.Prepend>
-		// 		<InputGroup.Text className="green text-light">{props.name}</InputGroup.Text>
-		// 	</InputGroup.Prepend>
-		// 	<FormControl as="textarea" aria-label="With textarea" />
-		// </InputGroup>
 	);
 }
 
--- src/components/InputTextForm/index.js
+++ src/components/InputTextForm/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { InputGroup, FormControl, Form } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import "./style.css";
 
 function InputTextForm(props) {
@@ -10,20 +10,6 @@ function InputTextForm(props) {
 				<Form.Control type={props.type} placeholder={props.placeholder} />
 			</Form.Group>
 		</Form>
-		// <InputGroup className="mb-3">
-		// 	<InputGroup.Prepend>
-		// 		<InputGroup.Text
-		// 			id="inputGroup-sizing-default"
-		// 			className=" text-light green"
-		// 		>
-		// 			{props.name}
-		// 		</InputGroup.Text>
-		// 	</InputGroup.Prepend>
-		// 	<FormControl
-		// 		aria-label="Default"
-		// 		aria-describedby="inputGroup-sizing-default"
-		// 	/>
-		// </InputGroup>
 	);
 }
 
